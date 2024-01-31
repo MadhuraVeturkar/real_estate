34e06c21b729fc8faf40e6de899f93488486d4c7
Chetan Rathod <chetan0101career@gmail.com>
2024-01-31 14:11:56 +0530

    auth done all

--- makaan/urls.py
+++ makaan/urls.py
@@ -6,5 +6,11 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name="index" ),
+    path('signup',views.signup, name="signup" ),
+    path('login',views.login, name="login" ),
+    path('logout',views.logout, name="logout" ),
+    path('add_property',views.add_property, name="add_property" ),
+    path('about',views.about, name="about" ),
+    path('contact',views.contact, name="contact" ),
     # path('search/<str:search_type>/<int:id>',views.search, name="search" ),
 ]
--- makaan/views.py
+++ makaan/views.py
@@ -1,5 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def index(request):
     return render(request,'index.html',{})
+
+def add_property(request):
+    return render(request,'add_property.html',{})
+
+def about(request):
+    return render(request,'about.html',{})
+
+def contact(request):
+    return render(request,'contact.html',{})
+
+
+def signup(request):
+    if request.method== "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['pwd']
+        
+        if User.objects.filter(username=email).exists():
+            # user already exist
+            return redirect('/signup')
+        else:
+            user = User.objects.create_user(username=email,password= password, email=email, first_name=name)
+            user.save()
+            # user created
+            return redirect('/login')
+    else:
+        return render(request, "sign_up.html", {})
+
+def login(request):
+    if request.method== "POST":
+        email = request.POST['email']
+        password = request.POST['pwd']
+        user= auth.authenticate(username=email,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            print(f"User {email} is logged in")
+            return redirect("/")
+        else:
+            print("Invalid Login detaisl")
+            return redirect("/login")
+    else:
+        return render(request, "login.html",{})
+    
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth.logout(request)
+    return redirect("/")
+
