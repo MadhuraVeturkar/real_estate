a427b254442d677f59f95bacc697ee39c7f0aabd
Chetan Rathod <chetan0101career@gmail.com>
2024-01-31 18:36:50 +0530

    properties on index page

--- makaan/models.py
+++ makaan/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Properties(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    types_list = [
+        ('apartment', 'Apartment'),
+        ('villa', 'Villa'),
+        ('home', 'Home'),
+        ('office', 'Office'),
+        ('building', 'Building'),
+        ('townhose', 'Townhouse'),
+        ('shop', 'Shop'),
+        ('garage', 'Garage'),
+        ]
+    property_type = models.CharField(max_length=50, choices=types_list)
+    status_list = [('on_sale', 'On Sale'), ('on_rent', 'On Rent')]
+    property_status = models.CharField(max_length=50, choices=status_list)
+    location = models.CharField(max_length=100)
+    bedrooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    floors = models.IntegerField()
+    garages = models.IntegerField()
+    area = models.FloatField()
+    price= models.FloatField()
+    map_link = models.CharField(max_length = 500)
+    address= models.CharField(max_length = 500)
+    image = models.ImageField(upload_to='property_img')
+
+    def __str__(self):
+        return self.title
+
--- makaan/views.py
+++ makaan/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from .models import Properties
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html',{})
+    properties = Properties.objects.all()
+    return render(request,'index.html',{"properties":properties})
 
 def add_property(request):
+    if request.method == "POST" and request.FILES['image']:
+        title = request.POST['title']
+        description = request.POST['description']
+        property_type = request.POST['type']
+        status = request.POST['status']
+        location = request.POST['location']
+        bedrooms = request.POST['bedrooms']
+        bathrooms = request.POST['bathrooms']
+        floors = request.POST['floors']
+        garages = request.POST['garages']
+        area = request.POST['area']
+        price = request.POST['price']
+        map_link = request.POST['map_link']
+        address = request.POST['address']
+        image = request.FILES['image']
+
+        new_pro = Properties.objects.create(title=title, description=description, property_type=property_type, property_status=status, location=location, bedrooms=bedrooms,bathrooms=bathrooms,floors=floors,garages=garages,area=area,price=price,map_link=map_link,address=address,image=image)
+
+        print("New Property Created !!!",new_pro)
+
+        return redirect('/add_property')
     return render(request,'add_property.html',{})
 
 def about(request):
